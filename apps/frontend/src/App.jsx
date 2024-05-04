c9db98704ff7c4afbb0da42d6a3b0f1ddb8373e3
Muamh <hussain.m@cerebotani.it>
2024-05-04 19:50:03 +0200

    api in afrontend updated

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [vall, setvall] = useState("");
 
+  useEffect(() => {
+    fetch('/api')
+      .then(response => response.text())
+      .then(data => setvall(data))
+      .catch(error => {
+        console.log(error);
+        setvall("Errore");
+      });
+  }, []);
+
+  
   return (
     <>
       <div>
@@ -27,6 +39,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+      </p>
+      <p className="read-the-docs">
+        qui {vall}
       </p>
     </>
   )
